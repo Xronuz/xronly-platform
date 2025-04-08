9b2d243576be7c2f7787ddddb5e48e10a357065e
xronuz <xronuz@gmail.com>
2025-04-08 14:21:24 +0500

    telegram connection error fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ import Navbar from "@/components/layout/Navbar"
 export default function Home() {
   return (
     <main className="bg-gradient-mesh text-white min-h-screen relative overflow-hidden">
-      <div className="absolute inset-0 bg-gradient-radial from-primary-500/10 to-transparent opacity-50"></div>
-      <Navbar />
-      <SplineSceneDemo />
+<div className="absolute inset-0 bg-gradient-radial from-primary-500/10 to-transparent opacity-50"></div>
+<Navbar />
+<MyNewComponent />
     </main>
   )
 }
